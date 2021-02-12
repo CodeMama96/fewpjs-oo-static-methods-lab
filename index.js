ae743ee40bf8cff6ee4d96a1a989447bf4c294a1
Alicia Weenum <weenum.alicia@gmail.com>
2021-02-12 14:36:32 -0500

    commit tree b00fb455df6ab5acd46a384ac9eae821507344a0

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 class Formatter {
-  //add static methods here
+
+  static capitalize (string){
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+
+  static sanitize(){
+    // return remove all non-alphaumeric characters except (/, '', " ")
+  }
+
+  static titleize(string){
+    //titleCase 
+    return string.replace(/\b\w/g, l => l.toUpperCase())
+    //const exception = ("the", "a", "an", "but", "of", "and", "for", "at", "by", "from")
+    //exception.ignore?
+  }
 }
